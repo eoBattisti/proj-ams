712351ce616f4d881aa12128bda4ed7fef1c31e7
Nicolas Battisti <nickgabrielbd@gmail.com>
2024-10-23 15:08:14 -0300

    feat(auth): add base auth requirements

--- django/core/views.py
+++ django/core/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import RedirectView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class HomeRedirectView(LoginRequiredMixin, RedirectView):
+    """
+    Redirects the user to the clients list view if he is logged in.
+    """
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse_lazy('clients:list')
