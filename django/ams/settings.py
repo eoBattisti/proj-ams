f81b6ae7b1cd2dfd42484e13be746d6efe8c4fe2
Nicolas Battisti <nickgabrielbd@gmail.com>
2024-12-09 17:49:24 -0300

    fix: add application environment variable

--- django/ams/settings.py
+++ django/ams/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e&d_(y+$9ix)7=@oq7i18e4hevyaxg7(o@_(je&dwdgx4n!w08"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
