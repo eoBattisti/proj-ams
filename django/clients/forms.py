bb49fcc6e11b76301e68217e190e4c04cd328e99
djoninhaA <djonathan8016@gmail.com>
2024-12-12 20:12:29 -0300

    fix(formating): adjustments pipeline

--- django/clients/forms.py
+++ django/clients/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Address, Client
 import re
 
+
 class ClientForm(forms.ModelForm):
     # Campos de endereço adicionados ao formulário
     street = forms.CharField(max_length=100, required=True)
@@ -11,63 +12,61 @@ class ClientForm(forms.ModelForm):
 
     class Meta:
         model = Client
-        fields = ['name', 'phone', 'annotations', 'street', 'number', 'neighborhood', 'city']
+        fields = ["name", "phone", "annotations", "street", "number", "neighborhood", "city"]
 
     def clean_name(self):
-        name = self.cleaned_data.get('name')
+        name = self.cleaned_data.get("name")
         if not name:
             raise forms.ValidationError('O campo "Nome" não pode estar vazio.')
         return name
 
     def clean_phone(self):
-        phone = self.cleaned_data.get('phone')
+        phone = self.cleaned_data.get("phone")
         if not phone:
             raise forms.ValidationError('O campo "Telefone" não pode estar vazio.')
 
-        pattern = r'^\(\d{2}\)\s?\d{4,5}-\d{4}$'
+        pattern = r"^\(\d{2}\)\s?\d{4,5}-\d{4}$"
 
         if not re.match(pattern, phone):
-            raise forms.ValidationError('O telefone deve estar no formato (XX) XXXXX-XXXX.')
+            raise forms.ValidationError("O telefone deve estar no formato (XX) XXXXX-XXXX.")
         return phone
 
     def clean_number(self):
-        number = self.cleaned_data.get('number')
+        number = self.cleaned_data.get("number")
         if number <= 0:
             raise forms.ValidationError('O numero deve ser diferente de 0')
         return number
 
     def clean_street(self):
-        street = self.cleaned_data.get('street')
+        street = self.cleaned_data.get("street")
         if not street:
             raise forms.ValidationError('O campo "Rua" não pode estar vazio.')
         return street
 
     def clean_neighborhood(self):
-        neighborhood = self.cleaned_data.get('neighborhood')
+        neighborhood = self.cleaned_data.get("neighborhood")
         if not neighborhood:
             raise forms.ValidationError('O campo "Bairro" não pode estar vazio.')
         return neighborhood
 
     def clean_city(self):
-        city = self.cleaned_data.get('city')
+        city = self.cleaned_data.get("city")
         if not city:
             raise forms.ValidationError('O campo "Cidade" não pode estar vazio.')
         return city
 
     def save(self, commit=True):
-        # Criação do endereço
         address = Address.objects.create(
-            street=self.cleaned_data['street'],
-            number=self.cleaned_data['number'],
-            neighborhood=self.cleaned_data['neighborhood'],
-            city=self.cleaned_data['city']
+            street=self.cleaned_data["street"],
+            number=self.cleaned_data["number"],
+            neighborhood=self.cleaned_data["neighborhood"],
+            city=self.cleaned_data["city"]
         )
 
-        # Criação do cliente, mas não salva ainda
         client = super().save(commit=False)
-        client.address = address  # Associa o novo endereço ao cliente
+        client.address = address
 
         if commit:
-            client.save()  # Salva o cliente com o endereço associado
+            client.save()
 
         return client
--- django/clients/views.py
+++ django/clients/views.py
@@ -6,28 +6,32 @@ from django.urls import reverse_lazy
 from .models import Client
 from .forms import ClientForm
 
+
 class ClientListView(LoginRequiredMixin, ListView):
     model = Client
     template_name = "clients/list.html"
     context_object_name = "clients"
+
 
 class ClientDetailView(LoginRequiredMixin, DetailView):
     model = Client
     template_name = "clients/detail.html"
     context_object_name = "client"
 
+
 class ClientCreateView(LoginRequiredMixin, CreateView):
     model = Client
     template_name = "clients/create.html"
     form_class = ClientForm
-    success_url = reverse_lazy('list')
-    # fields = ['name', 'phone', 'annotations', 'address']
+    success_url = reverse_lazy("list")
+
 
 class ClientUpdateView(LoginRequiredMixin, UpdateView):
     model = Client
     template_name = "clients/update.html"
-    fields = ['name', 'phone', 'annotations']
+    fields = ["name", "phone", "annotations"]
+
 
 class ClientDeleteView(LoginRequiredMixin, DeleteView):
     model = Client
-    success_url = reverse_lazy('clients:list')
+    success_url = reverse_lazy("clients:list")
