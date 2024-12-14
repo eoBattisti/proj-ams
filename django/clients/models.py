e1737c918b8d4f2e0dfe0e1d141cf0f130d22395
djoninhaA <djonathan8016@gmail.com>
2024-12-14 09:40:36 -0300

    feat(models): adding some verbose names in the client models

--- django/clients/models.py
+++ django/clients/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 from core.models import AbstractBaseModel
+from django.utils.translation import gettext_lazy as _
 
 
 class Address(models.Model):
-    street = models.CharField(max_length=100)
-    number = models.IntegerField()
-    neighborhood = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
+    street = models.CharField(verbose_name=_("Rua"), max_length=100)
+    number = models.IntegerField(verbose_name=_("Numero"))
+    neighborhood = models.CharField(verbose_name=_("Bairro"), max_length=100)
+    city = models.CharField(verbose_name=_("Cidade"), max_length=100)
 
     def __str__(self):
         return f"{self.street}, {self.number}, {self.neighborhood} - {self.city}"
 
 
 class Client(AbstractBaseModel):
-    name = models.TextField()
-    phone = models.TextField()
-    annotations = models.TextField()
+    name = models.TextField(verbose_name=_("Nome"))
+    phone = models.TextField(verbose_name=_("Telefone"))
+    annotations = models.TextField(verbose_name=_("Anotações"))
 
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
 
