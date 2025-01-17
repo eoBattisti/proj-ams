58fc82f3640809159f4f483167a0f4639c81fbc0
Nicolas Battisti <nickgabrielbd@gmail.com>
2025-01-17 18:15:42 -0300

    feat: add a base template to the hmtl files

---
+++ django/tasks/views/__init__.py
@@ -0,0 +1,28 @@
+from tasks.views.tasks import (
+    TaskListView,
+    TaskDetailView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView
+)
+from tasks.views.task_type import (
+    TaskTypeDeleteView,
+    TaskTypeListView,
+    TaskTypeDetailView,
+    TaskTypeCreateView,
+    TaskTypeUpdateView
+)
+
+
+__all__ = [
+    "TaskListView",
+    "TaskDetailView",
+    "TaskCreateView",
+    "TaskUpdateView",
+    "TaskTypeListView",
+    "TaskTypeDetailView",
+    "TaskTypeCreateView",
+    "TaskTypeUpdateView",
+    "TaskDeleteView",
+    "TaskTypeDeleteView"
+]
