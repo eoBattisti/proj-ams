1e0e442c6b614ee9c85a84205040fb0db50adaf4
djoninhaA <djonathan8016@gmail.com>
2025-01-20 20:03:19 -0300

    Feat: Changing table icons

--- django/clients/urls.py
+++ django/clients/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("", ClientListView.as_view(), name="list"),
     path("<uuid:pk>/", ClientDetailView.as_view(), name="detail"),
     path("create/", ClientCreateView.as_view(), name="create"),
-    path("update/<int:pk>/", ClientUpdateView.as_view(), name="update"),
-    path("delete/<int:pk>/", ClientDeleteView.as_view(), name="delete"),
+    path("update/<uuid:pk>/", ClientUpdateView.as_view(), name="update"),
+    path("delete/<uuid:pk>/", ClientDeleteView.as_view(), name="delete"),
 ]
