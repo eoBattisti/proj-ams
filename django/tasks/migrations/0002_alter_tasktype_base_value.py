239b3b999b9b4c20d64935a821adc80a91df6872
Nicolas Battisti <nickgabrielbd@gmail.com>
2025-06-24 15:53:49 -0300

    style: formatted files

--- django/tasks/migrations/0002_alter_tasktype_base_value.py
+++ django/tasks/migrations/0002_alter_tasktype_base_value.py
@@ -5,15 +5,21 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('tasks', '0001_initial'),
+        ("tasks", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='tasktype',
-            name='base_value',
-            field=models.DecimalField(decimal_places=2, max_digits=10, validators=[django.core.validators.MinValueValidator(limit_value=0, message='The Value must be greater than 0')], verbose_name='Base Value'),
+            model_name="tasktype",
+            name="base_value",
+            field=models.DecimalField(
+                decimal_places=2,
+                max_digits=10,
+                validators=[
+                    django.core.validators.MinValueValidator(limit_value=0, message="The Value must be greater than 0")
+                ],
+                verbose_name="Base Value",
+            ),
         ),
     ]
